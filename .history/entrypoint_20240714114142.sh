580afcbb44f5b638054c781511851d3731d9f605
yummiesdelivery <yummiesdelivery@outlook.com>
2024-07-22 07:17:49 -0400

    First commit

--- .history/entrypoint_20240714114142.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#Inicia el servidor
-flask run --host=0.0.0.0 --port=5000
--- .history/entrypoint_20240714125348.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Espera a que PostgreSQL esté disponible ejecutando el script de Python
-python wait_for_postgres.py
-
-# Ejecuta la aplicación
-python app.py
